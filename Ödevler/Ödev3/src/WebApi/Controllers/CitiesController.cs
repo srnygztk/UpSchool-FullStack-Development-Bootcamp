6e2747c6c035339aeedade3980af815bc370b13c
Serenay Gözütok <74133492+srnygztk@users.noreply.github.com>
2023-04-10 23:25:39 +0300

    Add files via upload

---
+++ Ödevler/Ödev3/src/WebApi/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+using Application.Features.Cities.Commands.Add;
+using Application.Features.Cities.Queries.GetAll;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Filters;
+
+namespace WebApi.Controllers
+{
+    [ValidationFilter]
+    public class CitiesController : ApiControllerBase
+    {
+        [HttpPost]
+        
+        public async Task<IActionResult> AddAsync(CityAddCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPost("GetAll")]
+        public async Task<IActionResult> GetAllAsync(CityGetAllQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            return Ok(await Mediator.Send(new CityGetAllQuery(id,null)));
+        }
+    }
+}
